14c98f5b4224144294a48dd7b4a0121589e0d763
adnanibrahim2907 <adnanibrahim2907@gmail.com>
2024-04-26 14:37:34 +0100

    Update connection.php

--- onlineStore/connection.php
+++ onlineStore/connection.php
@@ -1,16 +1,11 @@
 <?php
 
-// Database connection for LocalHost 
-    // $dbhost = "localhost";
-    // $dbuser = "root";
-    // $dbpass = "";
-    // $db = "cs230_u230408";
-
-// Database connection for Maynooth
-    $dbhost = "webcourse.cs.nuim.ie";
-    $dbuser = "u230408";
-    $dbpass = "Ieg8ahdahcieviin";
-    $db = "cs230_u230408";
+// Database connection 
+// add your database details here
+    $dbhost = "";
+    $dbuser = "";
+    $dbpass = "";
+    $db = "";
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);  // Create a connection to the database
 
